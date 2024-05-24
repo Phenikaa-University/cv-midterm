9349357a359c826239a599678e04bca96863835a
Cuong Vuong <cngvng123@gmail.com>
2024-05-24 09:06:56 +0700

    update data_process

--- data/data_process.py
+++ data/data_process.py
@@ -1,14 +1,19 @@
 import cv2
+from imutils import contours
+
 
 def segment(result,cntrs,saveToPath,name):
     import os
     tempImage=result.copy()
     for i,c in enumerate(cntrs):
-        box = cv2.boundingRect(c)
-        x,y,w,h = box
-        temp=tempImage[y:y+h,x:x+w]
-        cv2.imwrite(os.path.join(saveToPath,name+f"{i}.png"),temp)
-        cv2.rectangle(result, (x, y), (x+w, y+h), (0, 0, 255), 2)
+        area = cv2.contourArea(c)
+        min_area_threshold = 500  # adjust this value based on your requirement
+        if area > min_area_threshold:
+            box = cv2.boundingRect(c)
+            x,y,w,h = box
+            temp=tempImage[y:y+h,x:x+w]
+            cv2.imwrite(os.path.join(saveToPath,name+f"{i}.png"),temp)
+            cv2.rectangle(result, (x, y), (x+w, y+h), (0, 0, 255), 2)
     return result
 
 
@@ -34,11 +39,13 @@ def clearPath(path):
 
 def main(path):
     import os
+    from imutils import contours
     img=cv2.imread(path["images"])
     # cropped=img[20:img.shape[0]-20,30:img.shape[1]-20]
-    contours=makeContours(img,(150,3))
+    cntrs=makeContours(img,(150,3))
+    (cnts, _) = contours.sort_contours(cntrs, method="left-to-right")
     # clearPath(path["lines"])
-    result=segment(img,contours,path["lines"],"line")
+    result=segment(img,cnts,path["lines"],"line")
     # clearPath(path["words"])
     count=0
     for file in sorted(os.listdir(path["lines"])):
