d6ab9c46570f5b446cedb3bb8ad7bef3e8b4752e
Cuong Vuong <cngvng123@gmail.com>
2024-05-24 08:49:11 +0700

    update inference

--- inferences.py
+++ inferences.py
@@ -1,7 +1,7 @@
 import torch
 from model.model import CNN
 from torchvision import transforms
-from PIL import Image, ImageOps, ImageChops
+import cv2
 
 SAVE_MODEL_PATH = "checkpoints/best_accuracy.pth"
 
@@ -12,17 +12,9 @@ class Predict():
         self.model.load_state_dict(torch.load(SAVE_MODEL_PATH, map_location=device))
         self.transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))])
 
-    def _centering_img(self, img):
-        left, top, right, bottom = img.getbbox()
-        w, h = img.size[:2]
-        shift_x = (left + (right - left) // 2) - w // 2
-        shift_y = (top + (bottom - top) // 2) - h // 2
-        return ImageChops.offset(img, -shift_x, -shift_y)
 
     def __call__(self, img):
-        img = ImageOps.invert(img)
-        img = self._centering_img(img)
-        img = img.resize((28, 28), Image.BICUBIC)  # resize to 28x28
+        img = cv2.resize(img, (28, 28))  # resize to 28x28
         tensor = self.transform(img)
         tensor = tensor.unsqueeze_(0)  # 1,1,28,28
 
